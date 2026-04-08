a27c24c792770c2773abb8beadbd694eb6ac61c0
Richard Rivera <60705214+RichardRiv@users.noreply.github.com>
2026-04-07 20:40:52 -0400

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        let fast = 0;
+        let slow = 0;
+        
+        while(true){
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+            
+            if(fast === slow) break;
+        }
+
+        let slow2 = 0;
+        while(true){
+            slow = nums[slow];
+            slow2 = nums[slow2];
+
+            if(slow === slow2) return slow;
+        }
+    }
+}
